58e054cf4557ea3fea4097bb3d11e4ee89440728
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 15:40:22 +0700

    feat: add register post request from react to the laravel api

--- react-user-app/src/pages/Register.jsx
+++ react-user-app/src/pages/Register.jsx
@@ -1,18 +1,50 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Register() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+
+  const { setUser, setToken } = useStateContext();
+
   const onSubmit = (ev) => {
     ev.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    };
+    axiosClient
+      .post("/register", payload)
+      .then(({ data }) => {
+        setToken(data.token);
+        setUser(data.user);
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status == 422) {
+          console.log(response.data.errors);
+        }
+      });
   };
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Register to the App!</h1>
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Password Confirmation" />
+          <input ref={nameRef} placeholder="Full Name" />
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <input
+            ref={passwordConfirmationRef}
+            type="password"
+            placeholder="Password Confirmation"
+          />
           <button className="btn btn-block">Register</button>
           <p className="message">
             Already Registered? <Link to="/login">Login!</Link>
