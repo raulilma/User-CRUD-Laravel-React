981fa51bd002d7cd015105e195d95f3e7666faab
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 17:46:53 +0700

    feat: read users API implementation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection.
+     */
+    public function index()
+    {
+        return UserResource::collection(
+            User::query()->orderBy('id', 'desc')->paginate(10)
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
