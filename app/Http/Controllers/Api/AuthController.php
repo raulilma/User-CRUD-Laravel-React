ea25e137a8ebe50e6fb710ffee8758704c2abff2
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 16:04:03 +0700

    feat: login API controller logic

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,12 +7,24 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use PDO;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)){
+            return response([
+                'message' => 'Provided credentials for email address or password are invalid'
+            ]);
+        }
+        /** @var User $user*/
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
     }
 
     public function register(RegisterRequest $request)
