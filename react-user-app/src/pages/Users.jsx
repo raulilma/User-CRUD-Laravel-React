4cdbf7fb34b3c132313236c7f4db9f99bf9c7029
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 20:18:10 +0700

    feat: enable success notification popup timeout after delete

--- react-user-app/src/pages/Users.jsx
+++ react-user-app/src/pages/Users.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axiosClient from "../axios-client";
 import { Link } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { setNotification } = useStateContext();
 
   useEffect(() => {
     getUsers();
@@ -29,6 +31,7 @@ export default function Users() {
       return;
     }
     axiosClient.delete(`/users/${u.id}`).then(() => {
+      setNotification("User was successfully deleted!");
       getUsers();
     });
   };
