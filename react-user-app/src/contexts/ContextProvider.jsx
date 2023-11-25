245af41584b04db3137280b1c69473e8b913ed51
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 15:05:38 +0700

    add: login page view

--- react-user-app/src/contexts/ContextProvider.jsx
+++ react-user-app/src/contexts/ContextProvider.jsx
@@ -11,7 +11,7 @@ export const ContextProvider = ({ children }) => {
   const [user, setUser] = useState({
     name: "Raul",
   });
-  const [token, _setToken] = useState(123);
+  const [token, _setToken] = useState(null);
 
   const setToken = (token) => {
     _setToken(token);
