e67b4069bdfa48d592d8d7de8bfb68968e4c976d
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 14:17:55 +0700

    add: routing components for multiple layout types g

--- react-user-app/src/router.jsx
+++ react-user-app/src/router.jsx
@@ -1,21 +1,44 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Users from "./pages/Users";
+import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login />,
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/users" />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+    ],
   },
   {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/users",
-    element: <Users />,
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
   },
   {
     path: "*",
