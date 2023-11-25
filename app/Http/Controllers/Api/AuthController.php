6cb05d01d11cdbbc3250e1f040a01df7fbc71c9e
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 16:01:08 +0700

    feat: register API controller logic

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+    
+    public function logout(Request $request)
+    {
+
+    }
+}
