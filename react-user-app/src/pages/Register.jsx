8ca0f35912334985f143fcf8b0397aebddf8e756
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 15:18:46 +0700

    add: register page view

--- react-user-app/src/pages/Register.jsx
+++ react-user-app/src/pages/Register.jsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function Register() {
-    return <div>Register</div>;
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+  };
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Register to the App!</h1>
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password Confirmation" />
+          <button className="btn btn-block">Register</button>
+          <p className="message">
+            Already Registered? <Link to="/login">Login!</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
