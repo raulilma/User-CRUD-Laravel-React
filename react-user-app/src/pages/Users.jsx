b6a98c7316a3839c89ffce4185aa59903dc6737c
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-11-25 17:39:43 +0700

    add: basic axios request for users list

--- react-user-app/src/pages/Users.jsx
+++ react-user-app/src/pages/Users.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
+
 export default function Users() {
-    return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = () => {
+    setLoading(true);
+    axiosClient
+      .get("/users")
+      .then(({ data }) => {
+        setLoading(false);
+        console.log(data);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+  return <div>Users</div>;
 }
